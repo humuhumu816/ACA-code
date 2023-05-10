298ce26b2f1e970fa42a4a62ce897c15fe6f1cdc
humuhumu816 <128639587+humuhumu816@users.noreply.github.com>
2023-05-10 12:16:46 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('the encryptor js file is loaded');
+
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
+
+//console.log(letters[0]; // a
+
+for (let i = 0 < 10; i 
